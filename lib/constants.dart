6b59ad4cd1e922b53ed271d4013f9817bf46b207
shobiga-sc <115551680+shobiga-sc@users.noreply.github.com>
2024-01-29 18:24:59 +0530

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+const Color correct = Colors.green;
+const Color incorrect = Colors.red;
+const Color neutral = Colors.white10;
+const Color quest = Color.fromARGB(255, 245, 242, 242);
+const Color background = Color.fromARGB(255, 171, 144, 244);
